e3fe39bfd6120f8c8665b91559067be712c4ed84
John Leverett <jleverett@gls.com>
2018-07-16 13:33:24 -0400

    added handler to grab value of key and place in display

--- assets/index.js
+++ assets/index.js
@@ -8,23 +8,44 @@ $(document).ready(function(){
        ["SPACE"]
    ];
 
+   var actionKeys = [
+       ["[","]"],
+       [";","'","ENTER",],
+       [",",".","SHIFT"]
+   ];
+
    //create for loop that goes over array and calls function to make each row of keys
    for (i=0; i < keys.length; i++){
        var row = i+1;
        createKeys(keys[i], row);
-       console.log(keys.length);
+       createActionKeys(actionKeys, row);
    }
 
    //create function that creates each row
    function createKeys (row2create, row) {
        for (j = 0; j < row2create.length; j++) {
            var button = $("<button>");
-           button.attr({"data-key": row2create[j], "class": "key"});
+           button.attr({"data-key": row2create[j], "class": "key btn btn-secondary"});
            button.text(row2create[j]);
            $("#" + row).append(button);
 
        }
    }
+
+   //create the function that creates the actions keys
+   function createActionKeys(key2create, row) {
+       for (j = 0; j < key2create.length; j++) {
+           var button = $("<button>");
+           button.attr({"data-key": key2create[j], "class": "key btn btn-warning"});
+           button.text(key2create[j]);
+           $("#" + row).append(button);
+       }
+   }
+
+   $(".key").click(function(){
+       console.log(this);
+       $("#display").append(this.innerText);
+   })
        //loop over the row2create array
        //for each index
        //create a dom button
